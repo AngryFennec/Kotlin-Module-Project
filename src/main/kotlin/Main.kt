bb915dae6120d13d0c87f45acef4e643156cc0b5
Angry Fennec <shellfix@gmail.com>
2023-03-22 19:04:42 +0300

    Главное меню

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,25 @@
+import java.util.Scanner
+
 fun main() {
-    // ваш код начнется здесь
-    // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-    println("Привет")
+    val scanner = Scanner(System.`in`)
+    val archives = intArrayOf(0, 1, 2)
+    while (true) {
+        println("Список архивов:")
+        println("0. Создать архив")
+        archives.forEachIndexed { index, archive -> println("${index + 1}. Архив ${index}") }
+        println("${archives.size + 1}. Выход")
+        val input = scanner.nextInt() // Добавить обработку нечислового ввода
+        when (input) {
+            0 -> {
+                println("Создаем архив")
+            }
+            in 1..archives.size -> {
+                println("Переходим в архив ${input-1}")
+            }
+            archives.size + 1 -> {
+                println("Выходим из программы")
+                return
+            }
+        }
+    }
 }
