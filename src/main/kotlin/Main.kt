99e34fc617187476c853a2cacf7408671b4456d7
Angry Fennec <shellfix@gmail.com>
2023-03-28 19:49:48 +0300

    Добавляет остальной функционал

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,28 @@
 import java.util.Scanner
 
 fun main() {
-    val scanner = Scanner(System.`in`)
-    val archives = intArrayOf(0, 1, 2)
+    val archiveList = ArchiveList()
     while (true) {
         println("Список архивов:")
         println("0. Создать архив")
-        archives.forEachIndexed { index, archive -> println("${index + 1}. Архив ${index}") }
-        println("${archives.size + 1}. Выход")
-        val input = scanner.nextInt() // Добавить обработку нечислового ввода
+        archiveList.archives.forEachIndexed { index, archive -> println("${index + 1}. Архив ${archive.name}") }
+        println("${archiveList.archives.size + 1}. Выход")
+        val input = Scanner(System.`in`).nextLine().toIntOrNull()
+        if (input == null) {
+            println("Нужно ввести число")
+            continue
+        }
+        input.toInt()
         when (input) {
             0 -> {
                 println("Создаем архив")
+                archiveList.addArchive()
             }
-            in 1..archives.size -> {
-                println("Переходим в архив ${input-1}")
+            in 1..archiveList.archives.size -> {
+                println("Переходим в архив ${archiveList.archives[input-1].name}")
+                archiveList.archives[input-1].showArchiveContent()
             }
-            archives.size + 1 -> {
+            archiveList.archives.size + 1 -> {
                 println("Выходим из программы")
                 return
             }
